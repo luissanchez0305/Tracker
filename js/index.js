8885c27b79aa83e766d6a1e53828108e14bf01bc
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2014-10-02 18:34:31 -0500

    correccion carga mapa

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ var app = {
 
 	    	var mapOptions = {
 	    	    center: myLatlng,
-	    	    zoom: 15
+	    	    zoom: 14
 	    	};
 
 	    	map = new google.maps.Map(document.getElementById(mapId), mapOptions);
